45158d581b66d864c0f781f5a5cc761693be5489
Kshitiz Gupta <kshitizg786@gmail.com>
2024-12-16 17:02:30 +0530

    pushing VUcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import VideoUpload from './components/VideoUpload'
 import './App.css'
 
 function App() {
@@ -8,7 +9,12 @@ function App() {
 
   return (
     <>
-        <h1>Firt apps</h1>
+    <div className="flex justify-center py-9">
+      <h1 className='text-5xl font-extrabold text-gray-700 dark:text-gray-100' >
+        Welcome to video STREAMING App
+      </h1>
+
+    </div>
 
         <VideoUpload />
     </>
